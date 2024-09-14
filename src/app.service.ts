0090d5890c7a4c0d131c53ab5246df0d6adefab1
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2024-09-14 05:47:20 -0600

    new welcome

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to the iTransition_Internship-task4 NestJS API!';
+    return 'Welcome to the iTransition_Internship-task4 NestJS ROR-API !!';
   }
 }
